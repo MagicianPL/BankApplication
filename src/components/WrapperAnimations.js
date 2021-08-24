62fb31d86b892c108591c300e2fc07acc31aa5f5
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-24 22:19:31 +0200

    adding three blocks for animation wrapper

--- src/components/WrapperAnimations.js
+++ src/components/WrapperAnimations.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import Block from "../Block";
 
 const WrapperAnimations = () => {
-return (
+  return (
     <div className="wrapper-animations">
-
+      <Block text={<h2>Hello World</h2>} />
+      <Block text={<h2>Hello World2</h2>} />
+      <Block addclass="block-three"text={<h2>Hello World3</h2>} />
     </div>
-)
+  );
 };
 
 export default WrapperAnimations;
