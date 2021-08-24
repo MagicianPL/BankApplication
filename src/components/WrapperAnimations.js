169ed8aa2b722f7d062283b40c086fa1348dabd4
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-24 23:03:26 +0200

    animations

--- src/components/WrapperAnimations.js
+++ src/components/WrapperAnimations.js
@@ -4,9 +4,9 @@ import Block from "../Block";
 const WrapperAnimations = () => {
   return (
     <div className="wrapper-animations">
-      <Block text={<h2>Hello World</h2>} />
-      <Block text={<h2>Hello World2</h2>} />
-      <Block addclass="block-three"text={<h2>Hello World3</h2>} />
+      <Block addclass="block-one" text={<h2>Hello World</h2>} />
+      <Block addclass="block-two" text={<h2>Hello World2</h2>} />
+      <Block addclass="block-three" text={<h2>Hello World3</h2>} />
     </div>
   );
 };
