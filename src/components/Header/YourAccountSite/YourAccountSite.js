1fb69d0592c0c3af8c3818f199f492332abe748c
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-28 21:20:13 +0200

    top bar on second site

--- src/components/Header/YourAccountSite/YourAccountSite.js
+++ src/components/Header/YourAccountSite/YourAccountSite.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Nav from "../../Nav/Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCog
+} from "@fortawesome/free-solid-svg-icons";
 
 const YourAccountSite = () => {
-  return <Nav />;
+  return (
+      <div className="top-bar">
+          <p>{<FontAwesomeIcon icon={faCog} />}Witaj, NICK</p>
+          <Nav />
+      </div>
+  )
 };
 
 export default YourAccountSite;
