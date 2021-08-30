156b66e615c3d94400952507c4875cdebd7b123f
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-30 18:47:58 +0200

    currencies from NBP API

--- src/components/Header/YourAccountSite/Currencies.js
+++ src/components/Header/YourAccountSite/Currencies.js
@@ -3,56 +3,17 @@ import React, { useState, useEffect } from "react";
 const Currencies = () => {
   const [currenciesObjects, setCurrenciesObjects] = useState([]);
 
- /* useEffect(() => {
-    fetch(
-      "https://exchange-rates.abstractapi.com/v1/live/?api_key=2249da7bff9a45dcacf4b37dbeb05c72&base=USD&target=PLN"
-    )
-      .then((resp) => resp.json())
+  useEffect(() => {
+    fetch("http://api.nbp.pl/api/exchangerates/tables/a")
+      .then((json) => json.json())
       .then((data) => {
-        console.log(data);
-        setCurrenciesObjects((prevState) => [...prevState, data]);
-      })
-      .then(setTimeout(eurOnPln, 2000));
-  }, []);*/
-
-  const eurOnPln = () => {
-    fetch(
-      "https://exchange-rates.abstractapi.com/v1/live/?api_key=2249da7bff9a45dcacf4b37dbeb05c72&base=EUR&target=PLN"
-    )
-      .then((resp) => resp.json())
-      .then((data) => {
-        setCurrenciesObjects((prevState) => [...prevState, data]);
-        console.log(data);
-        console.log(currenciesObjects);
-      })
-      .then(setTimeout(gbpOnPln, 2000));
-  };
-
-  const gbpOnPln = () => {
-    fetch(
-      "https://exchange-rates.abstractapi.com/v1/live/?api_key=2249da7bff9a45dcacf4b37dbeb05c72&base=GBP&target=PLN"
-    )
-      .then((resp) => resp.json())
-      .then((data) => console.log(data))
-      .then(setTimeout(chfOnPln, 2000));
-  };
-
-  const chfOnPln = () => {
-    fetch(
-      "https://exchange-rates.abstractapi.com/v1/live/?api_key=2249da7bff9a45dcacf4b37dbeb05c72&base=CHF&target=PLN"
-    )
-      .then((resp) => resp.json())
-      .then((data) => console.log(data))
-      .then(setTimeout(czkOnPln, 2000));
-  };
-
-  const czkOnPln = () => {
-    fetch(
-      "https://exchange-rates.abstractapi.com/v1/live/?api_key=2249da7bff9a45dcacf4b37dbeb05c72&base=CZK&target=PLN"
-    )
-      .then((resp) => resp.json())
-      .then((data) => console.log(data));
-  };
+        data[0].rates.map((obj) => {
+          if (obj.code === "USD" || obj.code === "GBP") {
+            setCurrenciesObjects((prev) => [...prev, obj]);
+          }
+        });
+      });
+  }, []);
 
   return (
     <section>
