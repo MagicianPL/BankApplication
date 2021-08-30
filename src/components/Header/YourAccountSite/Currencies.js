3eae555e2e9728e5bc6a371e4a8bd9dffca0c9a4
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-30 22:16:18 +0200

    couple of currencies in my div

--- src/components/Header/YourAccountSite/Currencies.js
+++ src/components/Header/YourAccountSite/Currencies.js
@@ -2,20 +2,29 @@ import React, { useState, useEffect } from "react";
 
 const Currencies = () => {
   const [currenciesObjects, setCurrenciesObjects] = useState([]);
+  const [date, setDate] = useState("");
 
   useEffect(() => {
     fetch("http://api.nbp.pl/api/exchangerates/tables/a")
       .then((json) => json.json())
       .then((data) => {
         console.log(data)
+        setDate(data[0].effectiveDate); 
         data[0].rates.map((obj) => {
           const {code} = obj;
           obj.id = code;
           if (
             code === "USD" ||
+            code === "HUF" ||
+            code === "CZK" ||
+            code === "UAH" ||
+            code === "RUB" ||
             code === "GBP" ||
             code === "CHF" ||
             code === "AUD" ||
+            code === "JPY" ||
+            code === "HRK" ||
+            code === "TRY" ||
             code === "CAD"
           ) {
             setCurrenciesObjects((prev) => [...prev, obj]);
@@ -27,14 +36,15 @@ const Currencies = () => {
   return (
     <section>
       <div className="currencies">
-        <h2>Kursy walut na dzień 28.08.2021</h2>
-        {currenciesObjects.map((obj) => {
-          console.log(obj)
-          return(
-          <h4 key={obj.id}>
-            {obj.currency}  {obj.code} : {obj.mid}
-          </h4>)
-        })}
+        <h2>Kursy walut na dzień {date}</h2>
+        <div className="currencies-container">
+          {currenciesObjects.map((obj) => {
+            const { code, mid, currency } = obj;
+            return (<h4>
+              {currency} {code} : {mid}
+            </h4>);
+          })}
+        </div>
       </div>
     </section>
   );
