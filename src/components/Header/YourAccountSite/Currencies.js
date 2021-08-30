3ea4cc554f23a2f7022147c5c36a56b5f2f88979
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-30 19:44:41 +0200

    fun with nbp api

--- src/components/Header/YourAccountSite/Currencies.js
+++ src/components/Header/YourAccountSite/Currencies.js
@@ -7,8 +7,17 @@ const Currencies = () => {
     fetch("http://api.nbp.pl/api/exchangerates/tables/a")
       .then((json) => json.json())
       .then((data) => {
+        console.log(data)
         data[0].rates.map((obj) => {
-          if (obj.code === "USD" || obj.code === "GBP") {
+          const {code} = obj;
+          obj.id = code;
+          if (
+            code === "USD" ||
+            code === "GBP" ||
+            code === "CHF" ||
+            code === "AUD" ||
+            code === "CAD"
+          ) {
             setCurrenciesObjects((prev) => [...prev, obj]);
           }
         });
@@ -19,6 +28,13 @@ const Currencies = () => {
     <section>
       <div className="currencies">
         <h2>Kursy walut na dzień 28.08.2021</h2>
+        {currenciesObjects.map((obj) => {
+          console.log(obj)
+          return(
+          <h4 key={obj.id}>
+            {obj.currency}  {obj.code} : {obj.mid}
+          </h4>)
+        })}
       </div>
     </section>
   );
