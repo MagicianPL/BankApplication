c03776f25da4cc5b248edf47422e474c6eddf89e
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-05 21:26:06 +0200

    changed modal background and slide two is finished i think

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import SlideOne from "./SlideOne";
 import SlideTwo from "./SlideTwo";
 import SlideThree from "./SlideThree";
 
 const Header = () => {
-    return (
-        <header>
-           {/* <SlideOne />*/}
-            <SlideTwo />
-           {/*} <SlideThree />*/}
-        </header>
-    );
+  return (
+    <header>
+      <SlideOne />
+      <SlideTwo />
+      <SlideThree />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Header/YourAccountSite/MessageModal.js
+++ src/components/Header/YourAccountSite/MessageModal.js
@@ -8,7 +8,7 @@ const MessageModal = (props) => {
     <div className={`message-modal ${modalBoolean ? "" : "show-modal"}`}>
       <h2>OSTRZEŻENIE</h2>
       <p>
-        Przewalutowanie na walutę euro obciąży Twoje konto w kwocie{" "}
+        Przewalutowanie na walutę euro obciąży Twoje konto na kwotę{" "}
         <span>5 euro</span> - jest to całkowity koszt liczony po bieżącym kursie
       </p>
       <button className="cancel" onClick={() => props.function(true)}>
