b067849807285e46420b3210bd1e253d4337b5e0
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-05 20:47:38 +0200

    slider two text

--- src/components/Header/SlideTwo.js
+++ src/components/Header/SlideTwo.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 const SlideTwo = () => {
   return (
@@ -6,9 +8,15 @@ const SlideTwo = () => {
       <div className="flex-container">
         <p className="perspective">BŁYSKAWICZNE</p>
         <div className="flex-columns">
-          <p>Kredyty</p>
-          <p>Przelewy</p>
-          <p>Przewalutowania</p>
+          <p>
+            <FontAwesomeIcon icon={faBolt} /> Kredyty
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faBolt} /> Przelewy
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faBolt} /> Przewalutowania
+          </p>
         </div>
       </div>
     </div>
