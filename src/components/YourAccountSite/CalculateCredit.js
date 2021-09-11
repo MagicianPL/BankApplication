61875390dc2ab1402bcce4ec51f96de3d9a57d4f
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-11 16:03:49 +0200

    credit section its time for funcionality

--- src/components/YourAccountSite/CalculateCredit.js
+++ src/components/YourAccountSite/CalculateCredit.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRedo } from "@fortawesome/free-solid-svg-icons";
 
 import "./CalculateCredit.css";
 
 const CalculateCredit = () => {
+  const [showInstallmentInfo, setShowInstallmentInfo] = useState(false);
+  const [creditValue, setCreditValue] = useState("");
+  const [years, setYears] = useState("");
+  const [months, setMonths] = useState("");
   return (
     <section id="credit">
       <h2>
@@ -20,17 +24,60 @@ const CalculateCredit = () => {
       </p>
       <div className="input-section">
         <p>
-          Kwota pożyczki: <input type="number" min="100" placeholder="10000" />
+          Kwota pożyczki:{" "}
+          <input
+            type="number"
+            min="100"
+            placeholder="10000"
+            value={creditValue}
+            onChange={(e) => {
+              setCreditValue(e.target.value);
+            }}
+          />
           <span className="info">
             (Wpisz pełną kwotę bez znaków kropki czy przecinka)
           </span>
         </p>
         <p>
-          Chcę wziąć kredyt na <input type="number" min="0" placeholder="10" />{" "}
-          lat i <input type="number" min="0" placeholder="0" /> miesięcy
+          Chcę wziąć kredyt na{"\u00A0"}
+          <input
+            type="number"
+            min="0"
+            placeholder="10"
+            value={years}
+            onChange={(e) => {
+              setYears(e.target.value);
+            }}
+          />
+          {"\u00A0"}lat i{"\u00A0"}
+          <input
+            type="number"
+            min="0"
+            placeholder="0"
+            value={months}
+            onChange={(e) => {
+              setMonths(e.target.value);
+            }}
+          />{" "}
+          {"\u00A0"}miesięcy
         </p>
       </div>
-      <button><FontAwesomeIcon icon={faRedo} />PRZELICZ</button>
+
+      {showInstallmentInfo ? (
+        <div className="installment-info">
+          Jeśli przyznamy Ci XXX złotych kredytu na X lat i X miesięcy, Twoja
+          rata będzie wynosiła tylko XXX złotych miesięcznie
+        </div>
+      ) : null}
+
+      <button
+        onClick={() => {
+          setShowInstallmentInfo(true);
+        }}
+      >
+        <FontAwesomeIcon icon={faRedo} />
+        PRZELICZ
+      </button>
     </section>
   );
 };
