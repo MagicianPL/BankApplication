7f446b5ba36b8391918ea889183447930abd7a20
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-11 14:14:14 +0200

    credit section in progress

--- src/components/YourAccountSite/CalculateCredit.js
+++ src/components/YourAccountSite/CalculateCredit.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
 import "./CalculateCredit.css";
 
 const CalculateCredit = () => {
-return (
+  return (
     <section id="credit">
-
+      <h2>
+        Spełnij <span>MARZENIA</span> z kredytem gotówkowym w Banku u Skrudża
+      </h2>
+      <p>
+        Najlepszy możliwy wybór - oprocentowanie to tylko <span>6.5%</span> w
+        skali roku!
+      </p>
+      <p>
+        Wybierz kwotę pożyczki oraz czas spłaty, a my wyliczymy Twoją ratę
+        kredytu
+      </p>
+      <div className="input-section"></div>
     </section>
-)
+  );
 };
 
 export default CalculateCredit;
