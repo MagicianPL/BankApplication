ad978f675dc323166a0388a7f8e9d37cc2949671
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-15 18:46:48 +0200

    deposit-site in progress

--- src/components/DepositSite/DepositSite.js
+++ src/components/DepositSite/DepositSite.js
@@ -10,8 +10,20 @@ const DepositSite = (props) => {
         Ciebie!
       </h1>
       <div className="deposit">
-        <div className="choose-deposit"></div>
-        <div className="deposit-info"></div>
+        <div className="time-period">
+            <p className="mini-header">Na jaki okres lokujesz pieniądze?</p>
+            <p className="time">3 miesiące</p>
+            <p className="time">6 miesięcy</p>
+            <p className="time">12 miesięcy</p>
+            <p className="time">24 miesiące</p>
+            <p className="time">36 miesięcy</p>
+        </div>
+        <div className="money">
+            <input type="number"></input>
+        </div>
+        <div className="deposit-info">
+            mrkfkrfrmegrkegktrgktr g rtkgtr b ktrg ktr gktgjrngrt
+        </div>
       </div>
     </div>
   );
