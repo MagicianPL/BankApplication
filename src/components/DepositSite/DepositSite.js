e7f5c71e786a81e9d78cfdf9a3985f9e89a63df3
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-15 19:55:29 +0200

    buttons on deposit sites has values. Started writing function

--- src/components/DepositSite/DepositSite.js
+++ src/components/DepositSite/DepositSite.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./DepositSite.css";
 
 const DepositSite = (props) => {
+  const [monthsValue, setMonthsValue] = useState("");
+
+  const setMonths = (e) => {
+    console.log(e.target.value);
+    setMonthsValue(e.target.value);
+  };
+
   return (
     <div className="deposit-site">
       <h1>
-        <span>{props.login},</span> sprawdź jaką lokatę przygotowaliśmy specjalnie dla
-        Ciebie!
+        <span>{props.login},</span> sprawdź jaką lokatę przygotowaliśmy
+        specjalnie dla Ciebie!
       </h1>
       <div className="deposit">
         <div className="time-period">
-            <p className="mini-header">Na jaki okres lokujesz pieniądze?</p>
-            <p className="time">3 miesiące</p>
-            <p className="time">6 miesięcy</p>
-            <p className="time">12 miesięcy</p>
-            <p className="time">24 miesiące</p>
-            <p className="time">36 miesięcy</p>
+          <p className="mini-header">Na jaki okres lokujesz pieniądze?</p>
+          <button className="time" value="3" onClick={setMonths}>
+            3 miesiące
+          </button>
+          <button className="time" value="6" onClick={setMonths}>
+            6 miesięcy
+          </button>
+          <button className="time" value="12" onClick={setMonths}>
+            12 miesięcy
+          </button>
+          <button className="time" value="24" onClick={setMonths}>
+            24 miesiące
+          </button>
+          <button className="time" value="36" onClick={setMonths}>
+            36 miesięcy
+          </button>
         </div>
         <div className="money">
-            <p className="mini-header">Ile chcesz ulokować?</p>
-            <input type="number" />
-            <p>(Wpisz pełną kwotę bez znaków kropki czy przecinka)</p>
+          <p className="mini-header">Ile chcesz ulokować?</p>
+          <input type="number" />
+          <p>(Wpisz pełną kwotę bez znaków kropki czy przecinka)</p>
         </div>
         <div className="deposit-info">
-            mrkfkrfrmegrkegktrgktr g rtkgtr b ktrg ktr gktgjrngrt
+          Nieźle! Po wybranym okresie zarobisz XXX złotych!
         </div>
       </div>
     </div>
