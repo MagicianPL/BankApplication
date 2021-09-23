b0ad8a5130324d02aae83b958906909c8a2f0a8d
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-23 17:20:48 +0200

    inputs on transfer modal

--- src/components/BankTransfer/BankTransfer.js
+++ src/components/BankTransfer/BankTransfer.js
@@ -24,6 +24,20 @@ const BankTransfer = () => {
     <div className="bank-transfer-page">
         <div className="bank-transfer-container">
             <h1>PRZELEW<br />GOTÓWKOWY</h1>
+            <form>
+                <label htmlFor="recipient">Nazwa Odbiorcy</label>
+                    <input type="text" id="recipient" />
+                
+                <label htmlFor="account-number">Na konto</label>
+                    <input type="text" id="account-number" />
+
+                    <label htmlFor="transfer-title">Tytuł przelewu</label>
+                    <input type="text" id="transfer-title" />
+
+                    <label htmlFor="cash-amount">Na konto</label>
+                    <input type="number" id="cash-amount" />
+                
+            </form>
         </div>
      {/*} <input type="number" onChange={period} value={inputTransferValue}></input>
       <button onClick={validation}>wyslij</button>*/}
