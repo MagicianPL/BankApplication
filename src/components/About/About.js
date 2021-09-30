d2295d6cf4621021ff3181edfa505aa4dd768a23
Michał Sukowski <sukowskimichal@gmail.com>
2021-09-30 18:22:56 +0200

    text changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,8 @@ return (
         <div className="text">
             <p>Wiemy, że <span>zaufanie do Banku,</span> to trzon dobrej obsługi Klienta - ochrona Twoich funduszy jest dla nas najważniejsza.</p>
             <p>Chcemy także dać Ci możliwość oszczędzania czy podjęcia kredytu na bardzo dobrych warunkach. Od zawsze chcieliśmy być najlepsi i konkurencyjni dla innych banków.</p>
-            <p>Powstaliśmy w 1993 roku przy ul. Skąpców 100 w Warszawie i <span className="bold">zmieniamy świat na lepsze!</span></p>
+            <p>Powstaliśmy w 1993 roku przy ul. Skąpców 100 w Warszawie. </p>
+            <p className="bold">ZMIENIAMY ŚWIAT NA LEPSZE!</p>
         </div>
     </div>
 )
