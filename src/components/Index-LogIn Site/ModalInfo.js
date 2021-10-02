755afeed07059a1b52a832fc5455d8014aff4607
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-02 11:50:03 +0200

    modal info scrollbar

--- src/components/Index-LogIn Site/ModalInfo.js
+++ src/components/Index-LogIn Site/ModalInfo.js
@@ -1,17 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./ModalInfo.css";
 
 const ModalInfo = () => {
-    const [hideModal, setHideModal] = useState({});
+  const [hideModal, setHideModal] = useState({});
 
   return (
     <div className="modal-info" style={hideModal}>
       <div className="absolute-container">
         <h1>Witaj w mojej Aplikacji Bankowej,</h1>
         <p>
-          Zaloguj się używając <strong>dowolnego, wymyślonego loginu</strong><br />
-          oraz<br />
+          Zaloguj się używając <strong>dowolnego, wymyślonego loginu</strong>
+          <br />
+          oraz
+          <br />
           hasła: <strong>0 0 0 0</strong> (cztery zera).
         </p>
         <ul>
@@ -23,9 +25,13 @@ const ModalInfo = () => {
           <li>Wylicz kredyt</li>
           <li>Sprawdź lokatę!</li>
         </ul>
-        <button onClick={()=>{
-            setHideModal({display: "none"})
-        }}>SPRAWDZAM</button>
+        <button
+          onClick={() => {
+            setHideModal({ display: "none" });
+          }}
+        >
+          SPRAWDZAM
+        </button>
       </div>
     </div>
   );
